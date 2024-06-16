c6a04ba3bdb4689e04f9ae8eb9d4adbe5d86a509
Lucky alani <159769086+luckyalani@users.noreply.github.com>
2024-06-16 15:28:33 +0530

    Supere_Duper_Cerbosys
    Supere_Duper_Cerbosys

---
+++ superduper_user_html-main/header.js
@@ -0,0 +1,7 @@
+function openNav() {
+    let ele=  document.getElementById("navsidepage").style.display= "block";
+    if(ele=="block"){
+      document.getElementById("navsidepage").style.display= "none";
+      console.log("hi");
+              }
+     }
